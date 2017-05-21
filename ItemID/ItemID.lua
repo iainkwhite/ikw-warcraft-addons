52844d26c5143332c137114bf4c11ee7972c81a0
Iain White <iainkwhite@users.noreply.github.com>
2017-05-21 17:09:19 -0400

    Cope with items with no ItemID
    Cope with items with no ItemID   e.g. Mythic+ Keystones

--- ItemID/ItemID.lua
+++ ItemID/ItemID.lua
@@ -9,27 +9,31 @@ end
 
 function addline_itemid()
 	itemName,itemLink = ItemRefTooltip:GetItem()
-	if itemLink ~= nil then
+	if itemLink then	   
 		local itemString = string.match(itemLink, "item[%-?%d:]+")
-		local _, itemId, junk = strsplit(":", itemString)
-		local _, _, quality, ilevel = GetItemInfo(itemId)
-		ItemRefTooltip:AddLine("ItemID: |cFFFFFFFF"..itemId.."|cFFFFFF00  Base ilevel: |cFFFFFFFF"..(ilevel or "-"))
+		local _, itemId, junk = strsplit(":", itemString or "")
+		if itemId then
+			local _, _, quality, ilevel = GetItemInfo(itemId)
+			ItemRefTooltip:AddLine("ItemID: |cFFFFFFFF"..itemId.."|cFFFFFF00  Base ilevel: |cFFFFFFFF"..(ilevel or "-"))
+		else ItemRefTooltip:AddLine("No ItemID")
+		end
 		ItemRefTooltip:Show();
 	end
 end
 
 function addline_gametip()
 	itemName,itemLink = GameTooltip:GetItem()
-	if itemLink ~= nil then	   
+	if itemLink then	   
 		local itemString = string.match(itemLink, "item[%-?%d:]+")
-		local _, itemId, junk = strsplit(":", itemString)
-		local _, _, quality, ilevel = GetItemInfo(itemId)
-		GameTooltip:AddLine("ItemID: |cFFFFFFFF"..itemId.."|cFFFFFF00  Base ilevel: |cFFFFFFFF"..(ilevel or "-"))
+		local _, itemId, junk = strsplit(":", itemString or "") 
+		if itemId then
+			local _, _, quality, ilevel = GetItemInfo(itemId)
+			GameTooltip:AddLine("ItemID: |cFFFFFFFF"..itemId.."|cFFFFFF00  Base ilevel: |cFFFFFFFF"..(ilevel or "-"))
+		else GameTooltip:AddLine("No ItemID") 
+		end
 		GameTooltip:Show();
 	end
 end
-
+		
 ItemRefTooltip:HookScript("OnTooltipSetItem",addline_itemid)
 GameTooltip:HookScript("OnTooltipSetItem",addline_gametip)
-
-
